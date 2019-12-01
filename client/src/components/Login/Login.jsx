b11cf662be7620243614d761dabac8c94f551bb1
BuiTuanVu <vubui2010@gmail.com>
2019-12-01 12:11:51 +0700

    Change UI Login Page to use Ant Design

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,118 +1,104 @@
 import React from 'react';
-import {Redirect} from 'react-router-dom';
-import FacebookLogin from 'react-facebook-login';
+import { Route, Link } from 'react-router-dom';
+
+import 'antd/dist/antd.css';
+import './Login.css';
+import { Form, Icon, Input, Button, Checkbox, Tabs, Typography } from 'antd';
 import GoogleLogin from 'react-google-login';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import '../../App.css';
+import FacebookLogin from 'react-facebook-login';
 
-export default class Login extends React.Component {
-    constructor() {
-        super();
-        this.username = '';
-        this.password = '';
-        this.err = '';
-    }
+class LoginForm extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
 
-    render() {
-        const st = this.props;
-        if (st.token === 'err') {
-            this.err = 'Username or password is not correct !';
-        }
-        if (st.isLogin) {
-            return <Redirect to="/home"/>;
-        }
-        return (
-            <div className="loginLayout">
-                <Container component="main" maxWidth="xs">
-                    <CssBaseline/>
-                    <div className="paper">
-                        <center>
-                            <Avatar className="avatar1">
-                                <LockOutlinedIcon color="primary"/>
-                            </Avatar>
-                            <Typography component="h1" variant="h5">
-                                Sign in
-                            </Typography>
-                        </center>
-                        <form className="form" noValidate>
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                onChange={event => {
-                                    this.username = event.target.value;
-                                }}
-                                id="username"
-                                label="Username"
-                                name="username"
-                                autoFocus
-                            />
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                onChange={event => {
-                                    this.password = event.target.value;
-                                }}
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                            />
-                            <div className="Error">{this.err}</div>
-                            <Button
-                                fullWidth
-                                variant="contained"
-                                color="primary"
-                                onClick={event => {
-                                    event.preventDefault();
-                                    st.Login(this.username, this.password);
-                                }}
-                            >
-                                Sign In
-                            </Button>
-                            <br/><br/>
-                            <div style={{display: 'flex', flexWrap: 'wrap'}}>
-                                <FacebookLogin
-                                    appId="468555120306852"
-                                    fields="name,email,picture"
-                                    callback={(res) => {
-                                        st.LoginFB(res)
-                                    }}
-                                    cssClass="my-facebook-button-class"
-                                    icon="fa-facebook"
-                                    textButton="&nbsp;&nbsp;&nbsp;Sign In with Facebook"
-                                />&nbsp;&nbsp;&nbsp;
-                                <GoogleLogin
-                                    clientId="986011408031-fcsncbckfjdtv8085179rfu1n6p70psf.apps.googleusercontent.com"
-                                    onSuccess={(res) => {
-                                        st.LoginGG(res)
-                                    }}
-                                    className="btnGoogle">
-                                    <span>Sign In with Google</span>
-                                </GoogleLogin>
-                            </div>
-                            <Grid className="footer-login" container>
-                                <Grid item>
-                                    <Link href="/register" variant="body2">
-                                        Don&apos;t have an account? Sign Up
-                                    </Link>
-                                </Grid>
-                            </Grid>
-                        </form>
-                    </div>
-                </Container>
-            </div>
-        );
-    }
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div className="login-form">
+        <Typography>
+          <Typography.Title level={3} style={{ marginBottom: '20px' }}>
+            Đăng nhập
+          </Typography.Title>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Item>
+              {getFieldDecorator('username', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Xin điền tài khoản để đăng nhập!'
+                  }
+                ]
+              })(
+                <Input
+                  size="large"
+                  prefix={
+                    <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
+                  }
+                  placeholder="Nhập tài khoản email"
+                />
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('password', {
+                rules: [
+                  { required: true, message: 'Xin điền mật khẩu của bạn!' }
+                ]
+              })(
+                <Input
+                  size="large"
+                  prefix={
+                    <Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />
+                  }
+                  type="password"
+                  placeholder="Nhập mật khẩu"
+                />
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('remember', {
+                valuePropName: 'checked',
+                initialValue: true
+              })(<Checkbox style={{ float: 'left' }}>Remember me</Checkbox>)}
+              <Link className="login-form-forgot" to="/forgot-pass">
+                Quên mật khẩu?
+              </Link>
+              <Button
+                size="large"
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Đăng nhập
+              </Button>
+              <span>Bạn chưa có tài khoản? </span>
+              <Link to="/sign-up">Đăng ký ngay!</Link>
+            </Form.Item>
+          </Form>
+          <div className="btn-social">
+            <FacebookLogin
+              appId="1088597931155576"
+              fields="name,email,picture"
+              cssClass="facebook-btn"
+              icon="fa fa-facebook"
+              textButton="Đăng nhập bằng Facebook"
+            />
+            <GoogleLogin
+              className="google-btn"
+              clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+              buttonText="Đăng nhập bằng Google"
+              cookiePolicy={'single_host_origin'}
+            />
+          </div>
+        </Typography>
+      </div>
+    );
+  }
 }
+
+export const Login = Form.create({ name: 'normal_login' })(LoginForm);
