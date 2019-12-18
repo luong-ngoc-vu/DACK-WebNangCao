ab92f953e59da4901d895ea05dc09cfe4a7b184a
BuiTuanVu <vubui2010@gmail.com>
2019-12-19 01:40:05 +0700

    fix err line in root component

--- client/src/components/DetailTutor/InteractForm/InteractForm.jsx
+++ client/src/components/DetailTutor/InteractForm/InteractForm.jsx
@@ -8,91 +8,98 @@ import HireOrderContainer from '../../HireOrder/HireOrderContainer';
 import Chat from '../../Chat/Chat';
 
 class ContactForm extends React.Component {
-	state = {
-		loading: false,
-		visibleContract: false,
-		visibleChat: false
-	};
-	handleCancelContract = (e) => {
-		console.log(e);
-		this.setState({
-			visibleContract: false
-		});
-	};
-	handleCancelChat = (e) => {
-		console.log(e);
-		this.setState({
-			visibleChat: false
-		});
-	};
-	showContract = () => {
-		this.setState({
-			visibleContract: true
-		});
-	};
-	showChat = () => {
-		this.setState({
-			visibleChat: true
-		});
-	};
+  state = {
+    loading: false,
+    visibleContract: false,
+    visibleChat: false
+  };
+  handleCancelContract = e => {
+    console.log(e);
+    this.setState({
+      visibleContract: false
+    });
+  };
+  handleCancelChat = e => {
+    console.log(e);
+    this.setState({
+      visibleChat: false
+    });
+  };
+  showContract = () => {
+    this.setState({
+      visibleContract: true
+    });
+  };
+  showChat = () => {
+    this.setState({
+      visibleChat: true
+    });
+  };
 
-	render() {
-		const { visibleContract, visibleChat, loading } = this.state;
-		const st = this.props;
-		return (
-			<div className="side-form-contact">
-				{st.isLogin === true &&
-				st.typeUser === 1 && (
-					<Button size="large" type="primary" className="contact-button" onClick={this.showContract}>
-						<Icon type="solution" />
-						Thuê gia sư
-					</Button>
-				)}
+  render() {
+    const { visibleContract, visibleChat, loading } = this.state;
+    const st = this.props;
+    return (
+      <div className="side-form-contact">
+        {st.isLogin === true && st.typeUser === 1 && (
+          <Button
+            size="large"
+            type="primary"
+            className="contact-button"
+            onClick={this.showContract}
+          >
+            <Icon type="solution" />
+            Thuê gia sư
+          </Button>
+        )}
 
-				{st.isLogin === true &&
-				st.typeUser === 1 && (
-					<Button size="large" type="primary" className="contact-button" onClick={this.showChat}>
-						<Icon type="wechat" />
-						Chat với gia sư
-					</Button>
-				)}
+        {st.isLogin === true && st.typeUser === 1 && (
+          <Button
+            size="large"
+            type="primary"
+            className="contact-button"
+            onClick={this.showChat}
+          >
+            <Icon type="wechat" />
+            Chat với gia sư
+          </Button>
+        )}
 
-				{st.isLogin === true &&
-				st.typeUser === 1 && (
-					<Button size="large" type="primary" className="contact-button">
-						<Icon type="heart" /> Lưu
-					</Button>
-				)}
-				{st.isLogin === false && (
-					<Button size="large" type="primary" className="contact-button">
-						<Link to="/login">
-							<Icon type="heart" /> Login để thuê gia sư
-						</Link>
-					</Button>
-				)}
+        {st.isLogin === true && st.typeUser === 1 && (
+          <Button size="large" type="primary" className="contact-button">
+            <Icon type="heart" /> Lưu
+          </Button>
+        )}
+        {st.isLogin === false && (
+          <Button size="large" type="primary" className="contact-button">
+            <Link to="/login">
+              <Icon type="heart" /> Login để thuê gia sư
+            </Link>
+          </Button>
+        )}
 
-				{/* show modal */}
-				<Modal
-					visible={visibleContract}
-					style={{ top: 20, width: 600, minWidth: 600 }}
-					title="Gửi yêu cầu thuê gia sư"
-					onCancel={this.handleCancelContract}
-					footer={null}
-				>
-					<HireOrderContainer />
-				</Modal>
+        {/* show modal */}
+        <Modal
+          visible={visibleContract}
+          style={{ top: 20, width: 600, minWidth: 600 }}
+          title="Gửi yêu cầu thuê gia sư"
+          onCancel={this.handleCancelContract}
+          footer={null}
+        >
+          <HireOrderContainer />
+        </Modal>
 
-				<Modal
-					title="Basic Modal"
-					visible={this.state.visibleChat}
-					onCancel={this.handleCancelChat}
-					footer={null}
-				>
-					<Chat />
-				</Modal>
-			</div>
-		);
-	}
+        <Modal
+          title="Basic Modal"
+          visible={visibleChat}
+          onCancel={this.handleCancelChat}
+          footer={null}
+        >
+          <Chat />
+        </Modal>
+      </div>
+    );
+  }
 }
 
 const Contact = Form.create({ name: 'contact_form' })(ContactForm);
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,42 +4,46 @@ import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import throttle from 'lodash/throttle';
 import { applyMiddleware, createStore } from 'redux';
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 import indexReducer from './components/RootReducer';
 import Container from './components/RootContainer';
 
 const loadState = () => {
-	try {
-		const serializedState = localStorage.getItem('state');
-		if (serializedState === null) {
-			return undefined;
-		}
-		return JSON.parse(serializedState);
-	} catch (e) {
-		return undefined;
-	}
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (e) {
+    return undefined;
+  }
 };
 
-const saveState = (state) => {
-	try {
-		const serializedState = JSON.stringify(state);
-		localStorage.setItem('state', serializedState);
-	} catch (e) {}
+const saveState = state => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (e) {}
 };
 
 const peristedState = loadState();
 
-const store = createStore(indexReducer, peristedState, applyMiddleware(thunk));
+const store = createStore(
+  indexReducer,
+  peristedState,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 store.subscribe(
-	throttle(() => {
-		saveState(store.getState());
-	}, 1000)
+  throttle(() => {
+    saveState(store.getState());
+  }, 1000)
 );
 
 ReactDOM.render(
-	<Provider store={store}>
-		<Container />
-	</Provider>,
-	document.getElementById('root')
+  <Provider store={store}>
+    <Container />
+  </Provider>,
+  document.getElementById('root')
 );
