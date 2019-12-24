c9f90673facb01b979c8e08c8f6cae33ea8e3bf4
BuiTuanVu <vubui2010@gmail.com>
2019-12-24 22:18:55 +0700

    update function view skills and update skill of admin side

--- admin/src/components/ViewDetailSkill/ViewDetailSkill.jsx
+++ admin/src/components/ViewDetailSkill/ViewDetailSkill.jsx
@@ -1,79 +1,78 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './ViewDetailSkill.css';
-import {Button, Form, Input, Typography} from 'antd';
-import {Redirect} from "react-router-dom";
+import { Button, Form, Input, Typography } from 'antd';
+import { Redirect } from 'react-router-dom';
 
-const {Title} = Typography;
-
+const { Title } = Typography;
 
 class ProfileForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.name = '';
-        this.msg = '';
-    }
+	constructor(props) {
+		super(props);
+		this.name = '';
+		this.msg = '';
+	}
 
-    render() {
-        const st = this.props;
-        if (st.checkUpdateSkill === true) {
-            this.msg = 'Cập nhật kỹ năng thành công !';
-        }
-        if (st.isLogin === false) {
-            return <Redirect to="/admin-login"/>;
-        }
-        this.name = st.name;
-        return (
-            <div>
-                <Typography className="typo-data">
-                    <div>
-                        <Title level={4}>Chỉnh sửa thông tin của kỹ năng <b>{st.name}</b></Title>
-                        <br/>
-                        <Form
-                            style={{padding: '0px 50px'}}
-                            layout="vertical">
-                            <Form.Item label="Tên kỹ năng">
-                                <Input
-                                    size="large"
-                                    onChange={event => {
-                                        this.name = event.target.value;
-                                    }}
-                                    name="name"
-                                />
-                            </Form.Item>
-                            <Form.Item>
-                                <div
-                                    style={{
-                                        display: 'flex',
-                                        flexDirection: 'row',
-                                        justifyItems: 'center',
-                                        alignItems: 'center'
-                                    }}
-                                ><b>{this.msg}</b>
-                                </div>
-                            </Form.Item>
-                            <Form.Item>
-                                <Button
-                                    size="large"
-                                    type="primary"
-                                    htmlType="submit"
-                                    className="btn-sign-up"
-                                    onClick={event => {
-                                        event.preventDefault();
-                                        st.updateSkill(st._id, this.name);
-                                    }}
-                                >
-                                    Cập nhật
-                                </Button>
-                            </Form.Item>
-                        </Form>
-                    </div>
-                </Typography>
-            </div>
-        )
-            ;
-    }
+	render() {
+		const st = this.props;
+		if (st.checkUpdateSkill === true) {
+			this.msg = 'Cập nhật kỹ năng thành công !';
+		}
+		if (st.isLogin === false) {
+			return <Redirect to="/admin-login" />;
+		}
+		this.name = st.name;
+		return (
+			<div>
+				<Typography className="typo-data">
+					<div>
+						<Title style={{ textAlign: 'center' }} level={4}>
+							Cập nhật kỹ năng <b>{st.name}</b>
+						</Title>
+						<br />
+						<Form style={{ padding: '0px 50px' }} layout="vertical">
+							<Form.Item label="Tên kỹ năng">
+								<Input
+									size="large"
+									onChange={(event) => {
+										this.name = event.target.value;
+									}}
+									name="name"
+								/>
+							</Form.Item>
+							<Form.Item>
+								<div
+									style={{
+										display: 'flex',
+										flexDirection: 'row',
+										justifyItems: 'center',
+										alignItems: 'center'
+									}}
+								>
+									<b>{this.msg}</b>
+								</div>
+							</Form.Item>
+							<Form.Item>
+								<Button
+									size="large"
+									type="primary"
+									htmlType="submit"
+									className="btn-sign-up"
+									onClick={(event) => {
+										event.preventDefault();
+										st.updateSkill(st._id, this.name);
+									}}
+								>
+									Cập nhật
+								</Button>
+							</Form.Item>
+						</Form>
+					</div>
+				</Typography>
+			</div>
+		);
+	}
 }
 
-const ProfileHirer = Form.create({name: 'profile_form'})(ProfileForm);
+const ProfileHirer = Form.create({ name: 'profile_form' })(ProfileForm);
 export default ProfileHirer;
--- admin/src/components/ViewListSkill/ViewListSkill.jsx
+++ admin/src/components/ViewListSkill/ViewListSkill.jsx
@@ -1,140 +1,205 @@
 import React from 'react';
 // import './CreateAdmin.css';
 import 'antd/dist/antd.css';
-import {Button, Form, Input, Modal, Select, Table, Typography} from 'antd';
-import {Link, Redirect} from 'react-router-dom';
+import { Button, Form, Input, Modal, Select, Table, Typography } from 'antd';
+import { Link, Redirect } from 'react-router-dom';
 import CreateSkillContainer from '../CreateSkill/CreateSkillContainer';
-
-const {Title, Text} = Typography;
-const {Option} = Select;
-const {Search} = Input;
+import moment from 'moment';
+import ViewDetailSkillContainer from '../ViewDetailSkill/ViewDetailSkillContainer';
+const { Title, Text } = Typography;
+const { Option } = Select;
+const { Search } = Input;
 
 const columns = [
-    {
-        title: 'Tên kỹ năng',
-        dataIndex: 'skillName'
-    },
-    {
-        title: 'Thao tác',
-        dataIndex: 'action'
-    }
+	{
+		title: 'Tên kỹ năng',
+		dataIndex: 'skillName'
+	},
+	{
+		title: 'Ngày tạo',
+		dataIndex: 'createDate'
+	},
+	{
+		title: 'Thao tác',
+		dataIndex: 'action'
+	}
 ];
 
 class ViewListSkillForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            checkSearchByname: false,
-            dataSkill: [],
-            dataSkillByName: [],
-            visible: false
-        };
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			checkSearchByname: false,
+			dataSkill: [],
+			dataSkillByName: [],
+			visibleAdd: false,
+			visibleEdit: false
+		};
+	}
 
-    showModal = () => {
-        this.setState({
-            visible: true
-        });
-    };
-    handleOk = e => {
-        console.log(e);
-        this.setState({
-            visible: false
-        });
-    };
+	showModalEdit = () => {
+		this.setState({
+			visibleEdit: true
+		});
+	};
+	handleOkEdit = (e) => {
+		console.log(e);
+		this.setState({
+			visibleEdit: false
+		});
+	};
 
-    handleCancel = e => {
-        console.log(e);
-        this.setState({
-            visible: false
-        });
-    };
+	handleCancelEdit = (e) => {
+		console.log(e);
+		this.setState({
+			visibleEdit: false
+		});
+	};
 
-    componentDidMount() {
-        fetch('http://localhost:4000/admin/skills')
-            .then(response => response.json())
-            .then(data => this.setState({dataSkill: data}));
-    }
+	showModalAdd = () => {
+		this.setState({
+			visibleAdd: true
+		});
+	};
+	handleOkAdd = (e) => {
+		console.log(e);
+		this.setState({
+			visibleAdd: false
+		});
+	};
 
-    render() {
-        const st = this.props;
-        if (st.isLogin === false) {
-            return <Redirect to="/admin-login"/>;
-        }
-        const {dataSkill, dataSkillByName} = this.state;
-        let data_skill = dataSkill;
-        if (this.state.checkSearchByname === true)
-            data_skill = dataSkillByName;
-        else if (this.state.checkSearchByname === false)
-            data_skill = dataSkill;
-        const data = data_skill.map(row => ({
-            skillName: row.name,
-            action: (
-                <span style={{display: 'flex', flexDirection: 'row'}}>
-          <Link
-              style={{marginRight: 10}}
-              to="/admin-normal/detailSkill"
-              size="large"
-              onClick={event => {
-                  st.viewDetailSkill(row._id);
-              }}
-          >
-            <Button type="primary" icon="edit"/>
-          </Link>
+	handleCancelAdd = (e) => {
+		console.log(e);
+		this.setState({
+			visibleAdd: false
+		});
+	};
 
-          <Link>
-            <Button
-                icon="delete"
-                size="default"
-                onClick={event => {
-                    st.deleteSkill(row._id);
-                }}
-                type="danger"
-            />
-          </Link>
-        </span>
-            )
-        }));
-        return (
-            <div>
-                <div style={{width: '100%', padding: '5px 10px'}}>
-                    <Text style={{fontSize: '20px'}}>Danh sách kỹ năng</Text>
-                    <div style={{float: 'right'}}>
-                        <Search
-                            placeholder="Tìm kỹ năng"
-                            onSearch={value => {
-                                fetch(`http://localhost:4000/admin/getSkillByName/${value}`)
-                                    .then(response => response.json())
-                                    .then(data => this.setState({
-                                        dataSkillByName: data,
-                                        checkSearchByname: true
-                                    }));
-                            }}
-                            enterButton
-                            style={{width: 200, marginRight: '10px'}}
-                        />
+	componentDidMount() {
+		fetch('http://localhost:4000/admin/skills')
+			.then((response) => response.json())
+			.then((data) => this.setState({ dataSkill: data }));
+	}
 
-                        <Button type="primary" icon="plus" onClick={this.showModal}>
-                            Thêm kỹ năng
-                        </Button>
-                    </div>
-                </div>
-                <Table columns={columns} dataSource={data} size="default"/>
+	render() {
+		const st = this.props;
+		if (st.isLogin === false) {
+			return <Redirect to="/admin-login" />;
+		}
 
-                <Modal
-                    style={{width: 'auto', maxWidth: 400}}
-                    title="Tạo mới kỹ năng"
-                    visible={this.state.visible}
-                    onOk={this.handleOk}
-                    onCancel={this.handleCancel}
-                    footer={null}
-                >
-                    <CreateSkillContainer/>
-                </Modal>
-            </div>
-        );
-    }
+		const { dataSkill, dataSkillByName } = this.state;
+
+		let data_skill = dataSkill;
+
+		if (this.state.checkSearchByname === true) data_skill = dataSkillByName;
+		else if (this.state.checkSearchByname === false) data_skill = dataSkill;
+		let data = data_skill.map((item) => ({
+			skillName: item.name,
+			createDate: moment(item.createdDate).format('DD/MM/YYYY'),
+			key: item.name,
+			action: (
+				<span style={{ display: 'flex', flexDirection: 'row' }}>
+					<Button
+						style={{ marginRight: 10 }}
+						onClick={(event) => {
+							this.showModalEdit();
+							st.viewDetailSkill(item._id);
+						}}
+						type="primary"
+						icon="edit"
+					/>
+
+					<Button
+						icon="delete"
+						size="default"
+						onClick={(event) => {
+							st.deleteSkill(item._id);
+						}}
+						type="danger"
+					/>
+				</span>
+			),
+			children: item.children.map((children) => ({
+				skillName: children,
+				createDate: moment(item.createdDate).format('DD/MM/YYYY'),
+				key: children,
+				action: (
+					<span style={{ display: 'flex', flexDirection: 'row' }}>
+						<Button
+							style={{ marginRight: 10 }}
+							onClick={(event) => {
+								this.showModalEdit();
+								st.viewDetailSkill(item._id);
+							}}
+							type="primary"
+							icon="edit"
+						/>
+
+						<Button
+							icon="delete"
+							size="default"
+							onClick={(event) => {
+								st.deleteSkill(item._id);
+							}}
+							type="danger"
+						/>
+					</span>
+				)
+			}))
+		}));
+
+		return (
+			<div>
+				<div style={{ width: '100%', padding: '5px 10px' }}>
+					<Text style={{ fontSize: '20px' }}>Danh sách kỹ năng</Text>
+					<div style={{ float: 'right' }}>
+						<Search
+							placeholder="Tìm kỹ năng"
+							onSearch={(value) => {
+								fetch(`http://localhost:4000/admin/getSkillByName/${value}`)
+									.then((response) => response.json())
+									.then((data) =>
+										this.setState({
+											dataSkillByName: data,
+											checkSearchByname: true
+										})
+									);
+							}}
+							enterButton
+							style={{ width: 200, marginRight: '10px' }}
+						/>
+
+						<Button type="primary" icon="plus" onClick={this.showModalAdd}>
+							Thêm kỹ năng
+						</Button>
+					</div>
+				</div>
+				<Table columns={columns} dataSource={data} size="default" pagination={{ pageSize: 5 }} />
+
+				<Modal
+					style={{ width: 'auto', maxWidth: 450, minWidth: 450, top: 25 }}
+					title="Tạo mới kỹ năng"
+					visible={this.state.visibleAdd}
+					onOk={this.handleOkAdd}
+					onCancel={this.handleCancelAdd}
+					footer={null}
+				>
+					<CreateSkillContainer />
+				</Modal>
+				<Modal
+					style={{ width: 'auto', maxWidth: 450, minWidth: 450 }}
+					title="Cập nhật kỹ năng"
+					visible={this.state.visibleEdit}
+					onOk={this.handleOkEdit}
+					onCancel={this.handleCancelEdit}
+					footer={null}
+				>
+					<ViewDetailSkillContainer />
+				</Modal>
+			</div>
+		);
+	}
 }
 
-const ViewListSkill = Form.create({name: 'profile_form'})(ViewListSkillForm);
+const ViewListSkill = Form.create({ name: 'profile_form' })(ViewListSkillForm);
 export default ViewListSkill;
